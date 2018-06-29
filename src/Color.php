f3c3b876e703ffc33bff80cf2c9d0583093ccb76
Jitendra Adhikari <jiten.adhikary@gmail.com>
2018-06-29 19:02:02 +0700

    refactor(color): add more colors, remove mute, add typehints, extend magic call support

--- src/Color.php
+++ src/Color.php
@@ -12,70 +12,86 @@ namespace Ahc\Cli;
  */
 class Color
 {
-    const FG_RED    = 31;
-    const FG_GREEN  = 32;
-    const FG_YELLOW = 33;
-    const FG_BLUE   = 36;
-    // @todo
+    const BLACK  = 30;
+    const RED    = 31;
+    const GREEN  = 32;
+    const YELLOW = 33;
+    const BLUE   = 34;
+    const PURPLE = 35;
+    const CYAN   = 36;
+    const WHITE  = 37;
 
+    /** @var string Cli format */
     protected static $format = "\033[:bold:;:fg:;:bg:m:text:\033[0m";
 
+    /** @var array Custom styles */
     protected static $styles = [];
 
-    protected static $muted = false;
-
-    public static function error($text, array $style = [], $eol = false)
+    public static function error(string $text, array $style = [], bool $eol = false)
     {
-        static::line($text, ['fg' => static::FG_RED] + $style, $eol);
+        return static::line($text, ['fg' => static::RED] + $style, $eol);
     }
 
-    public static function info($text, array $style = [], $eol = false)
+    public static function info(string $text, array $style = [], bool $eol = false)
     {
-        static::line($text, ['fg' => static::FG_BLUE] + $style, $eol);
+        return static::line($text, ['fg' => static::BLUE] + $style, $eol);
     }
 
-    public static function warn($text, array $style = [], $eol = false)
+    public static function warn(string $text, array $style = [], bool $eol = false)
     {
-        static::line($text, ['fg' => static::FG_YELLOW] + $style, $eol);
+        return static::line($text, ['fg' => static::YELLOW] + $style, $eol);
     }
 
-    public static function comment($text, array $style = [], $eol = false)
+    public static function comment(string $text, array $style = [], bool $eol = false)
     {
-        static::line($text, ['fg' => static::FG_RED] + $style, $eol);
+        return static::line($text, ['fg' => static::BLACK, 'bold' => 1] + $style, $eol);
     }
 
-    public static function line($text, array $style = [], $eol = false)
+    /**
+     * Returns a formatted/colored line.
+     *
+     * @param  string $text
+     * @param  array   $style
+     * @param  bool $eol End of line
+     *
+     * @return string
+     */
+    public static function line(string $text, array $style = [], bool $eol = false)
     {
-        if (static::$muted) {
-            return;
-        }
-
-        $style += ['bg' => null, 'fg' => 37, 'bold' => 0];
+        $style += ['bg' => null, 'fg' => static::WHITE, 'bold' => false];
 
         $format = $style['bg'] === null
             ? \str_replace(';:bg:', '', static::$format)
             : static::$format;
 
-        echo \strtr($format, [
+        $line = \strtr($format, [
             ':bold:' => (int) $style['bold'],
             ':fg:'   => (int) $style['fg'],
-            ':bg:'   => (int) $style['bg'],
+            ':bg:'   => (int) $style['bg'] + 10,
             ':text:' => (string) $text,
         ]);
 
         if ($eol) {
-            static::eol();
+            $line .= static::eol();
         }
+
+        return $line;
     }
 
     public static function eol()
     {
-        if (!static::$muted) {
-            echo PHP_EOL;
-        }
+        return PHP_EOL;
     }
 
-    public function style($name, array $style)
+    /**
+     * Register a custom style.
+     *
+     * @param string $name  Example: 'alert'
+     * @param array  $style Example: ['fg' => Color::RED, 'bg' => Color::YELLOW, 'bold' => true]
+     *
+     * @return void
+     */
+    public function style(string $name, array $style)
     {
         $allow = ['fg' => true, 'bg' => true, 'bold' => true];
         $style = \array_intersect_key($style, $allow);
@@ -91,32 +107,84 @@ class Color
         static::$styles[$name] = $style;
     }
 
-    public static function __callStatic($name, $arguments)
+    /**
+     * Magically build styles.
+     *
+     * @param  string $name      Example: 'boldError', 'bgGreenBold' etc
+     * @param  array  $arguments
+     *
+     * @return string
+     */
+    public static function __callStatic(string $name, array $arguments)
     {
-        if (empty($arguments[0])) {
+        if (!isset($arguments[0])) {
             throw new \InvalidArgumentException('Text required');
         }
 
-        list($text, $style, $eol) = $arguments + ['', [], false];
+        list($name, $text, $style, $eol) = static::parseCall($name, $arguments);
 
-        if (\substr($name, 0, 4) === 'bold') {
-            $name = \lcfirst(\substr($name, 4));
-            static::{$name}($text, ['bold' => true] + $style, $eol);
-
-            return;
+        if (isset(static::$styles[$name])) {
+            return static::line($text, $style + static::$styles[$name], $eol);
         }
 
-        if (!isset(static::$styles[$name])) {
+        if (\defined($color = static::class . '::' . \strtoupper($name))) {
+            $name   = 'line';
+            $style += ['fg' => \constant($color)];
+        }
+
+        if (!\method_exists(static::class, $name)) {
             throw new \InvalidArgumentException(\sprintf('Style %s not defined', $name));
         }
 
-        $style = static::$styles[$name];
-
-        static::line($text, $style, $eol);
+        return static::{$name}($text, $style, $eol);
     }
 
-    public static function mute($muted = true)
+    /**
+     * Parse the name argument pairs to determine callable method and style params.
+     *
+     * @param  string $name
+     * @param  array  $arguments
+     *
+     * @return array
+     */
+    protected function parseCall(string $name, array $arguments)
     {
-        static::$muted = $muted;
+        list($text, $style, $eol) = $arguments + ['', [], false];
+
+        if (\stripos($name, 'bold') !== false) {
+            $name   = \str_ireplace('bold', '', $name);
+            $style += ['bold' => 1];
+        }
+
+        if (!\preg_match_all('/([b|B|f|F]g)?([A-Z][a-z]+)([^A-Z])?/', $name, $matches)) {
+            return [\lcfirst($name) ?: 'line', $text, $style, $eol];
+        }
+
+        list($name, $style) = static::buildStyle($name, $style, $matches);
+
+        return [$name, $text, $style, $eol];
+    }
+
+    /**
+     * Build style parameter from matching combination.
+     *
+     * @param string $name
+     * @param array  $style
+     * @param array  $matches
+     *
+     * @return arrsy
+     */
+    protected function buildStyle(string $name, array $style, array $matches)
+    {
+        foreach ($matches[0] as $i => $match) {
+            $name  = \str_replace($match, '', $name);
+            $type  = \strtolower($matches[1][$i]) ?: 'fg';
+
+            if (\defined($color = static::class . '::' . \strtoupper($matches[2][$i]))) {
+                $style += [$type => \constant($color)];
+            }
+        }
+
+        return [\lcfirst($name) ?: 'line', $style];
     }
 }
