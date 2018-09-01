08744972f3625bc94cf2fdaf5edc3bcbb327372e
Sushil Gupta <desushil@gmail.com>
2018-09-01 19:21:23 +0545

    Style fixes - unindenting inside <?php tag

--- src/Helper/Shell.php
+++ src/Helper/Shell.php
@@ -1,151 +1,151 @@
 <?php
 
-    /*
-     * Shell wrapper for package adhocore/php-cli.
-     * @author Sushil Gupta <desushil@gmail.com>
-     * @license MIT
-     */
+/*
+ * Shell wrapper for package adhocore/php-cli.
+ * @author Sushil Gupta <desushil@gmail.com>
+ * @license MIT
+ */
 
-    namespace Ahc\Cli\Helper;
+namespace Ahc\Cli\Helper;
 
-    use Ahc\Cli\Exception\RuntimeException;
+use Ahc\Cli\Exception\RuntimeException;
 
-    class Shell {
+class Shell {
 
-        const STDIN_DESCRIPTOR_KEY = 0;
-        const STDOUT_DESCRIPTOR_KEY = 1;
-        const STDERR_DESCRIPTOR_KEY = 2;
+    const STDIN_DESCRIPTOR_KEY = 0;
+    const STDOUT_DESCRIPTOR_KEY = 1;
+    const STDERR_DESCRIPTOR_KEY = 2;
 
-        protected $command;
-        protected $descriptors;
-        protected $input;
-        protected $pipes;
-        protected $process;
-        protected $startTime;
-        protected $status;
-        protected $timeout;
+    protected $command;
+    protected $descriptors;
+    protected $input;
+    protected $pipes;
+    protected $process;
+    protected $startTime;
+    protected $status;
+    protected $timeout;
 
-        public function __construct(string $command, string $input = null, float $timeout = 10)
-        {
-            if (!\function_exists('proc_open')) {
-                throw new RuntimeException('Required proc_open could not be found in your PHP setup');
-            }
-
-            $this->command = $command;
-            $this->input = $input;
-            $this->timeout = $timeout;
-            $this->status = null;
+    public function __construct(string $command, string $input = null, float $timeout = 10)
+    {
+        if (!\function_exists('proc_open')) {
+            throw new RuntimeException('Required proc_open could not be found in your PHP setup');
         }
 
-        private function getDescriptors()
-        {
-            if ('\\' === \DIRECTORY_SEPARATOR) {
-                return array(
-                    self::STDIN_DESCRIPTOR_KEY => array('pipe', 'r'),
-                    self::STDOUT_DESCRIPTOR_KEY => array('file', 'NUL', 'w'),
-                    self::STDERR_DESCRIPTOR_KEY => array('file', 'NUL', 'w'),
-                );
-            } else {
-                return array(
-                    self::STDIN_DESCRIPTOR_KEY => array('pipe', 'r'),
-                    self::STDOUT_DESCRIPTOR_KEY => array('pipe', 'w'),
-                    self::STDERR_DESCRIPTOR_KEY => array('pipe', 'w'),
-                );
-            }
+        $this->command = $command;
+        $this->input = $input;
+        $this->timeout = $timeout;
+        $this->status = null;
+    }
+
+    private function getDescriptors()
+    {
+        if ('\\' === \DIRECTORY_SEPARATOR) {
+            return array(
+                self::STDIN_DESCRIPTOR_KEY => array('pipe', 'r'),
+                self::STDOUT_DESCRIPTOR_KEY => array('file', 'NUL', 'w'),
+                self::STDERR_DESCRIPTOR_KEY => array('file', 'NUL', 'w'),
+            );
+        } else {
+            return array(
+                self::STDIN_DESCRIPTOR_KEY => array('pipe', 'r'),
+                self::STDOUT_DESCRIPTOR_KEY => array('pipe', 'w'),
+                self::STDERR_DESCRIPTOR_KEY => array('pipe', 'w'),
+            );
+        }
+    }
+
+    private function setInput()
+    {
+        fwrite($this->pipes[self::STDIN_DESCRIPTOR_KEY], $this->input);
+    }
+
+    private function updateStatus()
+    {
+        $this->status = proc_get_status($this->process);
+
+        return $this->status;
+    }
+
+    private function closePipes()
+    {
+        fclose($this->pipes[self::STDIN_DESCRIPTOR_KEY]);
+        fclose($this->pipes[self::STDOUT_DESCRIPTOR_KEY]);
+        fclose($this->pipes[self::STDERR_DESCRIPTOR_KEY]);
+    }
+
+    public function execute()
+    {
+        if ($this->isRunning()) {
+            throw new RuntimeException('Process is already running');
         }
 
-        private function setInput()
-        {
-            fwrite($this->pipes[self::STDIN_DESCRIPTOR_KEY], $this->input);
+        $this->descriptors = $this->getDescriptors();
+
+        $this->process = proc_open($this->command, $this->descriptors, $this->pipes);
+
+        if (!\is_resource($this->process)) {
+            throw new RuntimeException('Bad program could not be started.');
         }
 
-        private function updateStatus()
-        {
-            $this->status = proc_get_status($this->process);
+        $this->setInput();
+        $this->updateStatus();
 
-            return $this->status;
+        $this->startTime = microtime(true);
+    }
+
+    public function getOutput()
+    {
+        return stream_get_contents($this->pipes[self::STDOUT_DESCRIPTOR_KEY]);
+    }
+
+    public function getErrorOutput()
+    {
+        return stream_get_contents($this->pipes[self::STDERR_DESCRIPTOR_KEY]);
+    }
+
+    public function getExitCode()
+    {
+        return $this->status['exitcode'];
+    }
+
+    public function isRunning()
+    {
+        if (!$this->status) {
+            return false;
         }
 
-        private function closePipes()
-        {
-            fclose($this->pipes[self::STDIN_DESCRIPTOR_KEY]);
-            fclose($this->pipes[self::STDOUT_DESCRIPTOR_KEY]);
-            fclose($this->pipes[self::STDERR_DESCRIPTOR_KEY]);
-        }
+        $this->updateStatus();
 
-        public function execute()
-        {
-            if ($this->isRunning()) {
-                throw new RuntimeException('Process is already running');
-            }
+        return $this->status['running'];
+    }
 
-            $this->descriptors = $this->getDescriptors();
+    public function getProcessId()
+    {
+        return $this->isRunning() ? $this->status['pid'] : null;
+    }
 
-            $this->process = proc_open($this->command, $this->descriptors, $this->pipes);
-
-            if (!\is_resource($this->process)) {
-                throw new RuntimeException('Bad program could not be started.');
-            }
-
-            $this->setInput();
-            $this->updateStatus();
-
-            $this->startTime = microtime(true);
-        }
-
-        public function getOutput()
-        {
-            return stream_get_contents($this->pipes[self::STDOUT_DESCRIPTOR_KEY]);
-        }
-
-        public function getErrorOutput()
-        {
-            return stream_get_contents($this->pipes[self::STDERR_DESCRIPTOR_KEY]);
-        }
-
-        public function getExitCode()
-        {
-            return $this->status['exitcode'];
-        }
-
-        public function isRunning()
-        {
-            if(!$this->status) {
-                return false;
-            }
-
-            $this->updateStatus();
-
-            return $this->status['running'];
-        }
-
-        public function getProcessId()
-        {
-            return $this->isRunning() ? $this->status['pid'] : null;
-        }
-
-        public function stop()
-        {
-            if (!$this->isRunning()) {
-                return $this->getExitCode();
-            }
-
-            $this->closePipes();
-
-            if (\is_resource($this->process)) {
-                proc_close($this->process);
-            }
-
+    public function stop()
+    {
+        if (!$this->isRunning()) {
             return $this->getExitCode();
         }
 
-        public function kill()
-        {
-            return proc_terminate($this->process);
+        $this->closePipes();
+
+        if (\is_resource($this->process)) {
+            proc_close($this->process);
         }
 
-        public function __destruct()
-        {
-            $this->stop();
-        }
+        return $this->getExitCode();
     }
+
+    public function kill()
+    {
+        return proc_terminate($this->process);
+    }
+
+    public function __destruct()
+    {
+        $this->stop();
+    }
+}
--- tests/Helper/ShellTest.php
+++ tests/Helper/ShellTest.php
@@ -1,29 +1,29 @@
 <?php
 
-    /*
-     * Test for shell wrapper for package adhocore/php-cli.
-     * @author Sushil Gupta <desushil@gmail.com>
-     * @license MIT
-     */
+/*
+ * Test for shell wrapper for package adhocore/php-cli.
+ * @author Sushil Gupta <desushil@gmail.com>
+ * @license MIT
+ */
 
-    namespace Ahc\Cli\Test\Helper;
+namespace Ahc\Cli\Test\Helper;
 
-    use Ahc\Cli\Helper\Shell;
-    use PHPUnit\Framework\TestCase;
+use Ahc\Cli\Helper\Shell;
+use PHPUnit\Framework\TestCase;
 
-    class ShellTest extends TestCase {
+class ShellTest extends TestCase {
 
-        public function test_get_output()
-        {
-            $shell = new Shell("echo hello");
-            $shell->execute();
-            $this->assertSame("hello\n", $shell->getOutput());
-        }
-
-        public function test_get_process_id()
-        {
-            $shell = new Shell("echo hello");
-            $shell->execute();
-            $this->assertInternalType("int", $shell->getProcessId());
-        }
+    public function test_get_output()
+    {
+        $shell = new Shell("echo hello");
+        $shell->execute();
+        $this->assertSame("hello\n", $shell->getOutput());
     }
+
+    public function test_get_process_id()
+    {
+        $shell = new Shell("echo hello");
+        $shell->execute();
+        $this->assertInternalType("int", $shell->getProcessId());
+    }
+}
