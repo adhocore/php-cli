aeaf5f48b52d6089ac3f7c9c9fa45ddd7aff7bc6
Jitendra Adhikari <jiten.adhikary@gmail.com>
2018-09-07 17:46:19 +0700

    refactor(plugin): phpcli => ahccli

--- ahccli.plugin.zsh
+++ ahccli.plugin.zsh
@@ -1,6 +1,6 @@
 # ------------------------------------------------------------------------------
-#          FILE:  phpcli.plugin.zsh
-#   DESCRIPTION:  oh-my-zsh phpcli plugin file.
+#          FILE:  ahccli.plugin.zsh
+#   DESCRIPTION:  oh-my-zsh ahccli plugin file.
 #        AUTHOR:  Jitendra Adhikari (jiten.adhikary@gmail.com)
 #       VERSION:  0.0.1
 #       LICENSE:  MIT
@@ -9,18 +9,18 @@
 #         Check https://github.com/adhocore/php-cli#autocompletion
 # ------------------------------------------------------------------------------
 
-# PHPCli command completion
-_phpcli_command_list () {
+# AhcCli command completion
+_ahccli_command_list () {
   command $1 --help 2>/dev/null | sed "1,/Commands/d" | grep -v Run | awk '/  [a-z]+ / { print $2 }'
 }
 
-# PHPCli option completion
-_phpcli_option_list () {
+# AhcCli option completion
+_ahccli_option_list () {
   command $1 $2 --help 2>/dev/null | sed '1,/Options/d' | gawk 'match($0, /  .*(--[a-z-]+)(\.\.\.)?.    /, o) { print o[1] }'
 }
 
-# PHPCli compdef handler
-_phpcli () {
+# AhcCli compdef handler
+_ahccli () {
   local curcontext="$curcontext" state line cmd subcmd
   typeset -A opt_args
   _arguments \
@@ -31,14 +31,14 @@ _phpcli () {
   subcmd=`echo $line | awk '{print $1}'`
 
   case $state in
-    cmd) compadd $(_phpcli_command_list $cmd) ;;
-    opts) compadd $(_phpcli_option_list $cmd $subcmd) ;;
+    cmd) compadd $(_ahccli_command_list $cmd) ;;
+    opts) compadd $(_ahccli_option_list $cmd $subcmd) ;;
   esac
 }
 
 #
 # Register commands for autocompletion below:
 #
-# format:  compdef _phpcli <cmd>
-# example: compdef _phpcli phint
+# format:  compdef _ahccli <cmd>
+# example: compdef _ahccli phint
 #
