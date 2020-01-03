c4799953c2a458bacc03805c7f6dfee3b5088f37
Jitendra Adhikari <jiten.adhikary@gmail.com>
2020-01-03 20:43:18 +0700

    feat(cmd.action): bind to $this

--- src/Input/Command.php
+++ src/Input/Command.php
@@ -413,7 +413,7 @@ class Command extends Parser
             return $this->_action;
         }
 
-        $this->_action = $action;
+        $this->_action = $action ? \Closure::bind($action, $this) : null;
 
         return $this;
     }
