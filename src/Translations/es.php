733285ebbfa0ff472fd067b7a3e4bb83ecfb9ece
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2024-11-29 19:52:51 +0100

    provides `fr`, `es`, `de` and `ru` translations

---
+++ src/Translations/es.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the PHP-CLI package.
+ *
+ * (c) Jitendra Adhikari <jiten.adhikary@gmail.com>
+ *     <https://github.com/adhocore>
+ *
+ * Licensed under MIT license.
+ */
+
+return [
+    // main
+    'arguments'          => 'Argumentos',
+    'choice'             => 'Elección',
+    'choices'            => 'Opciones (separadas por coma)',
+    'command'            => 'Comando',
+    'commandNotFound'    => 'Comando %s no encontrado',
+    'commandSuggestion'  => '¿Quiso decir %s?',
+    'commands'           => 'Comandos',
+    'descWithDefault'    => '%s [por defecto: %s]',
+    'helpExample'        => '[OPCIONES...] [ARGUMENTOS...]',
+    'optionHelp'         => 'Leyenda: <requerido> [opcional] variadic...',
+    'options'            => 'Opciones',
+    'promptInvalidValue' => 'Valor no válido. ¡Intente nuevamente!',
+    'showHelp'           => 'Mostrar ayuda',
+    'showHelpFooter'     => 'Ejecute `<comando> --help` para ayuda específica',
+    'showVersion'        => 'Mostrar versión',
+    'stackTrace'         => 'Rastro de pila',
+    'thrownIn'           => 'lanzado en',
+    'thrownAt'           => 'en',
+    'usage'              => 'Uso',
+    'usageExamples'      => 'Ejemplos de uso',
+    'version'            => 'versión',
+    'verbosityLevel'     => 'Nivel de verbosidad',
+
+    // translations
+    'argumentVariadic'           => 'Solo el último argumento puede ser variádico',
+    'badProgram'                 => 'No se pudo iniciar el programa defectuoso.',
+    'commandAlreadyAdded'        => 'El comando "%s" ya fue añadido',
+    'commandDoesNotExist'        => 'El comando "%s" no existe',
+    'configOptionMissing'        => 'Se requiere el valor de la opción de configuración',
+    'invalidTableRowsType'       => 'Las filas deben ser un array de arrays asociativos, %s dado',
+    'optionNotRegistered'        => 'La opción "%s" no está registrada',
+    'parameterAlreadyRegistered' => 'El parámetro "%s" ya está registrado',
+    'processAlreadyRun'          => 'El proceso ya está en ejecución.',
+    'procOpenMissing'            => 'La función proc_open no está disponible en su configuración PHP.',
+    'progressbarCurrentMax'      => 'El %d actual es mayor que el total %d.',
+    'progressbarTotalMin'        => 'El total de la barra de progreso debe ser mayor que cero.',
+    'styleInvisible'             => 'Los estilos incorporados no pueden ser invisibles',
+    'textRequired'               => 'Texto requerido',
+    'timeoutOccured'             => 'Tiempo agotado, proceso terminado.',
+    'undefinedStyle'             => 'Estilo "%s" no definido',
+    'usingInvalidStyle'          => 'Intentando establecer un estilo vacío o inválido'
+];
